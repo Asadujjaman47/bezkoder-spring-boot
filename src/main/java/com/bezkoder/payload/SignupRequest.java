d85bf3519bf8b37e02dc532de51f0872e5a6b1f8
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2024-09-15 19:09:35 +0600

    Use the Custom Validation

---
+++ src/main/java/com/bezkoder/payload/SignupRequest.java
@@ -0,0 +1,81 @@
+package com.bezkoder.payload;
+
+import com.bezkoder.validation.PasswordMatching;
+import com.bezkoder.validation.StrongPassword;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@PasswordMatching(
+        password = "password",
+        confirmPassword = "confirmPassword",
+        message = "Password and Confirm Password must be matched!"
+)
+public class SignupRequest {
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @StrongPassword
+    private String password;
+
+    private String confirmPassword;
+
+    public SignupRequest() {
+    }
+
+    public SignupRequest(String username, String email, String password, String confirmPassword) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public @NotBlank @Size(min = 3, max = 20) String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NotBlank @Size(min = 3, max = 20) String username) {
+        this.username = username;
+    }
+
+    public @NotBlank @Size(max = 50) @Email String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank @Size(max = 50) @Email String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "SignupRequest{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", confirmPassword='" + confirmPassword + '\'' +
+                '}';
+    }
+}
