ad8ce04cc178efb9d32357fab876c76ec7793ef9
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2024-09-19 16:22:43 +0600

    Create Repository Interfaces for One To One mapping

---
+++ src/main/java/com/bezkoder/repository/TutorialDetailsRepository.java
@@ -0,0 +1,15 @@
+package com.bezkoder.repository;
+
+import com.bezkoder.model.TutorialDetails;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TutorialDetailsRepository extends JpaRepository<TutorialDetails, Long> {
+    @Transactional
+    void deleteById(long id);
+
+    @Transactional
+    void deleteByTutorialId(long tutorialId);
+}
