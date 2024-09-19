560145c334e66ec35701887844d880492a11c15b
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2024-09-19 16:14:00 +0600

    Define Data Model for JPA One to One mapping

---
+++ src/main/java/com/bezkoder/model/Tutorial.java
@@ -0,0 +1,30 @@
+package com.bezkoder.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tutorials")
+public class Tutorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private boolean published;
+
+}
---
+++ src/main/java/com/bezkoder/model/TutorialDetails.java
@@ -0,0 +1,30 @@
+package com.bezkoder.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tutorial_details")
+public class TutorialDetails {
+
+    @Id
+    private Long id;
+
+    @Column
+    private Date createdOn;
+
+    @Column
+    private String createdBy;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "tutorial_id")
+    private Tutorial tutorial;
+}
