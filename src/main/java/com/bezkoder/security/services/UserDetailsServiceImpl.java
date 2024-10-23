f1325557dfc1c9cf160d0cfe66b6901b6803c4ca
Asad <asad1@itconquest.com>
2024-10-23 18:31:12 +0600

    Implement UserDetails & UserDetailsService

---
+++ src/main/java/com/bezkoder/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.bezkoder.security.services;
+
+import com.bezkoder.models.User;
+import com.bezkoder.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetailsImpl loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
