b76e2bbf41b3cdc90abe7168279d913a7fcaedd8
Asad <asad1@itconquest.com>
2025-01-20 14:54:59 +0600

    Pagination and Sorting

--- src/main/java/com/bezkoder/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/repository/TutorialRepository.java
@@ -1,11 +1,20 @@
 package com.bezkoder.repository;
 
 import com.bezkoder.model.Tutorial;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-    List<Tutorial> findByPublished(boolean published);
-    List<Tutorial> findByTitleContaining(String title);
+//    List<Tutorial> findByPublished(boolean published);
+//    List<Tutorial> findByTitleContaining(String title);
+
+    Page<Tutorial> findByPublished(boolean published, Pageable pageable);
+
+    Page<Tutorial> findByTitleContaining(String title, Pageable pageable);
+
+    List<Tutorial> findByTitleContaining(String title, Sort sort);
 }
